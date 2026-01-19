37ff7d8a47ef9953535bf8d2a91cbcfe1df75955
Juliano Galhardo de Oliveira <julianogalhardo@192.168.1.3>
2026-01-19 19:09:50 -0300

    feat: adding communication class

---
+++ Petfolio/Controllers/PetController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Petfolio.Communication.Requests;
+using Petfolio.Communication.Responses;
+
+namespace Petfolio.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PetController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseRegisterPetJson), StatusCodes.Status201Created)]
+        public IActionResult Register([FromBody] RequestRegisterPetJson request)
+        {
+            return Created(string.Empty, null);
+        }
+    }
+}
